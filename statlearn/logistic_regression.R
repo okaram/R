58a2be08f890756c560f53126ba7f8e12112ad26
Orlando Karam <orlando@thekarams.net>
2014-03-29 15:49:10 -0700

    more logistic regression

--- statlearn/logistic_regression.R
+++ statlearn/logistic_regression.R
@@ -12,3 +12,11 @@ logodds=function(p)
 {
   return (log(prob2odds(p)));
 }
+
+# pass the prob of NOT and the prob of YES
+logreg_coeffs=function(p0, p1)
+{
+  b0=logodds(p0);
+  b1=logodds(p1)-logodds(p0);
+  return (c(b0,b1));
+}
